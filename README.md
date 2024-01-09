aa6bc8fb0fdaf21806b7722e8675b675db8d5a68
Kcx520 <156074571+Kcx520@users.noreply.github.com>
2024-01-09 18:06:01 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+## My Own GHpages
+Used For Personaly
---
+++ index.js
@@ -0,0 +1,9 @@
+function redirectToBili() {
+    window.location.href = "https://space.bilibili.com/";
+}
+//点头像 开Bilibili
+
+function redirectToGithub() {
+    window.location.href = "https://github.com/"
+}
+//h1 title 开Github
