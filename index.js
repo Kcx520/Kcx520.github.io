05e1f279897f3a5a026d6adf493677a6cf1f44c3
zhou-hack <wangyimrzhou@163.com>
2024-01-09 22:49:43 +0800

    Updation

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function redirectToBili() {
-    window.location.href = "https://space.bilibili.com/";
+    window.location.href = "https://space.bilibili.com/1350489523";
 }
 //点头像 开Bilibili
 
 function redirectToGithub() {
-    window.location.href = "https://github.com/"
+    window.location.href = "https://github.com/kcx520"
 }
 //h1 title 开Github
